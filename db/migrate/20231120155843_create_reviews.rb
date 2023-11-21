c8ae78e0f196687ac09d4df117acb3d5d2d89a83
Gaelle Vivet <ga.vivet@gmail.com>
2023-11-21 17:50:17 +0100

    added google fonts and show view for lessons not finished

--- db/migrate/20231120155843_create_reviews.rb
+++ db/migrate/20231120155843_create_reviews.rb
@@ -4,7 +4,6 @@ class CreateReviews < ActiveRecord::Migration[7.1]
       t.text :comment
       t.references :booking, null: false, foreign_key: true
       t.timestamps
-      validates :comment, presence: true, length: { minimum: 10, maximum: 500 }
     end
   end
 end
